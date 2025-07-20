96a795f8f286e76893eefd9a6f07f217d0dcb160
Marek Toome <marekjah@gmail.com>
2025-07-20 07:24:12 +0300

    Day 28

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 90])
+
+<img src="http://picsum.photos/seed/{{ rand(0, 10000) }}/{{ $width }}/{{ $width }}.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU" alt="" class="rounded-xl">
