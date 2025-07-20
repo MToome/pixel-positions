208b0ea5e3be7f77f8099a5939743473f51aa1fa
Marek Toome <marekjah@gmail.com>
2025-07-20 10:39:39 +0300

    Day 29

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,3 +1,5 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
 
     <div>
@@ -6,17 +8,18 @@
 
     <div class="flex-1 flex flex-col">
 
-        <a href="#" class="self-start text-sm text-gray-400 ">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400 ">{{ $job->employer->name }}</a>
 
-        <h3 class="mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <h3 class="mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
 
-        <p class="text-sm text-gray-400 mt-auto">Full Time- From $60,000</p>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule . " - " . $job->salary }}</p>
 
     </div>
 
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach ($job->tags as $tag)
+            <x-tag :$tag size="small" />
+        @endforeach
+
     </div>
 </x-panel>
